b5009b34676f8732be2147ada7b7f0316a1d061e
reinadvorak <142573724+reinadvorak@users.noreply.github.com>
2024-02-27 15:10:14 -0800

    Loading

--- js/loading.js
+++ js/loading.js
@@ -14,6 +14,6 @@ function blurring() {
     loadText.innerText = `${load}%`
     loadText.style.opacity = 1 - load/100
 
-    bg.style.filter = `blur(${30 - (load/100*30)}px)`
+    bg.style.filter = `blur(${30-(load/100)*30}px)`
 
 }
