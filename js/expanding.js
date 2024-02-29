23cb44d94b8225a8e5764f672f5e958a33f30635
reinadvorak <142573724+reinadvorak@users.noreply.github.com>
2024-02-29 22:33:39 +0000

    Thu, Feb 29, 2024, 2:33 PM -08:00

--- js/expanding.js
+++ js/expanding.js
@@ -2,6 +2,13 @@ panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
+        removeActiveClasses()
         panel.classList.add('active')
     })
 })
+
+function removeActiveClasses(){
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
