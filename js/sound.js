517b7f0499eb023e0311201d06e111f6eddae452
reinadvorak <142573724+reinadvorak@users.noreply.github.com>
2024-03-01 22:39:57 +0000

    Fri, Mar 1, 2024, 2:39 PM -08:00

--- js/sound.js
+++ js/sound.js
@@ -2,5 +2,6 @@ sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
 sounds.forEach(sound => {
     btn = document.createElement('button')
-    
+    btn.classList.add('btn')
+    btn.innerText=sound
 })
