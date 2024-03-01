3998a5f7eaf42a14a08f45ba2445b54e9eebaeb1
reinadvorak <142573724+reinadvorak@users.noreply.github.com>
2024-03-01 22:41:47 +0000

    Fri, Mar 1, 2024, 2:41 PM -08:00

--- js/sound.js
+++ js/sound.js
@@ -4,4 +4,6 @@ sounds.forEach(sound => {
     btn = document.createElement('button')
     btn.classList.add('btn')
     btn.innerText=sound
+
+    document.getElementById('buttons').appendChild(btn)
 })
