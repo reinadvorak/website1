060fde37a2ff45eb9a506cbcd051a24d62e6ec5d
reinadvorak <142573724+reinadvorak@users.noreply.github.com>
2024-03-06 07:44:28 +0000

    Tue, Mar 5, 2024, 11:44 PM -08:00

--- js/navigation.js
+++ js/navigation.js
@@ -3,5 +3,9 @@ nav = document.querySelector('nav')
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
-    console.log("window.scrollY")
+    if (window.scrollY > 223) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
 }
