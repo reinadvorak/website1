7ba00337c631c0a5c747c48069a3d292ae17562c
reinadvorak <142573724+reinadvorak@users.noreply.github.com>
2024-03-15 21:51:35 +0000

    Fri, Mar 15, 2024, 2:51 PM -07:00

--- js/index.js
+++ js/index.js
@@ -3,4 +3,14 @@ container = document.getElementById('container')
 btn = document.getElementById('btn')
 close = document.getElementById('close')
 
+close.addEventListener('click', () => {
+    modal.classList.remove('show-modal')
+})
 
+open.addEventListener('click', () => {
+    container.classList.add('show-container')
+})
+
+close.addEventListener('click', () => {
+    container.classList.remove('show-container')
+})
