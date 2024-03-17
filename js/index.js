df7f9b2f3c916b7d1421fd4786de94b42cfc934d
reinadvorak <142573724+reinadvorak@users.noreply.github.com>
2024-03-16 19:00:52 -0700

    index

--- js/index.js
+++ js/index.js
@@ -5,16 +5,10 @@ close = document.getElementById('close')
 
 close.addEventListener('click', () => {
     btn.classList.add('show-btn')
-})
-
-open.addEventListener('click', () => {
     container.classList.add('show-container')
 })
 
-close.addEventListener('click', () => {
-    container.classList.remove('show-container')
-})
-
 open.addEventListener('click', () => {
+    container.classList.remove('show-container')
     btn.classList.remove('show-btn')
 })
